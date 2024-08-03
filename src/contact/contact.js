92eda0ef983a75c320f3331cc3fd653445036a6c
oi-taa <ishita21953@gmail.com>
2024-08-03 18:08:14 +0530

    Added email js functionality

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,23 +1,50 @@
 import React from "react";
 import "./contact.css";
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_wkhx4uy', 'template_hk19h7b', form.current, {
+        publicKey: 'p56ttZ13doUXLeh7I',
+      })
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+      e.target.reset()
+  };
   return (
     <div class="frame">
       <div class="container">
-        <div class="form-container">
-          <h2 class="form-title">Contact Us</h2>
-          <input class="form-input" placeholder="Name" type="name"></input>
-          <input class="form-input" placeholder="Email" type="email"></input>
-          <textarea class="form-textarea" placeholder="Message"></textarea>
-          <button class="form-button">Submit</button>
+        <div class="content-wrapper">
+          <div class="form-container">
+            <h2 class="form-title">Contact Us</h2>
+            <form ref={form} onSubmit={sendEmail}>
+              <input class="form-input" placeholder="Name" type="text" name="user_name"></input>
+              <input class="form-input" placeholder="Email" type="email" name="user_email"></input>
+              <textarea class="form-textarea" placeholder="Message" name="message"></textarea>
+              <button type="submit"  class="form-button">Submit</button>
+            </form>
+            
+          </div>
+          <div id="map" class="map-container">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.6267970225545!2d77.03537277528821!3d28.610970775676495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d05dd375e5a13%3A0x108adaa3abe4bd07!2sNetaji%20Subhas%20University%20of%20Technology!5e0!3m2!1sen!2sin!4v1718000812649!5m2!1sen!2sin"
+              title="Kalpana Map" width="100%" height="480" style={{ border: 0 }} allowfullscreen loading="lazy">
+            </iframe>
+          </div>
         </div>
-      </div>
-      <div id="map" class="map-container">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.6267970225545!2d77.03537277528821!3d28.610970775676495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d05dd375e5a13%3A0x108adaa3abe4bd07!2sNetaji%20Subhas%20University%20of%20Technology!5e0!3m2!1sen!2sin!4v1718000812649!5m2!1sen!2sin"
-          title="Kalpana Map" width="100%" height="480" style={{ border: 0 }} allowfullscreen="" loading="lazy">
-        </iframe>
       </div>
     </div>
   );
